6a32d3e160cb4a9cee8237482cb0d9f261f61698
Sergii Okhotnyi <okhotny@gmail.com>
2014-11-20 22:30:43 -0800

    iofabric

--- src/main/java/dev/okhotny/TVCalendar/App.java
+++ src/main/java/dev/okhotny/TVCalendar/App.java
@@ -2,9 +2,12 @@ package dev.okhotny.TVCalendar;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 import com.squareup.picasso.Picasso;
+
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
@@ -14,6 +17,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         sInstance = this;
         tracker = GoogleAnalytics.getInstance(this).newTracker(R.xml.global_tracker);
         if (BuildConfig.DEBUG) {
